3555d1ca23e50998adee17cb5e41014344def205
ed6dev <5adnanechetaini5@gmail.com>
2023-11-07 21:43:39 +0100

    removed some symbols :)

--- Quadratic-Equation/script.js
+++ Quadratic-Equation/script.js
@@ -11,21 +11,21 @@ document.getElementById("calc").addEventListener("submit", function (event) {
   if (D > 0) {
     var x1 = ((-b - Math.sqrt(D)) / (2 * a)).toFixed(2);
     var x2 = ((-b + Math.sqrt(D)) / (2 * a)).toFixed(2);
-    result = `[-] x1 = ${x1}`;
-    result2 = `[-] x2 = ${x2}`;
+    result = `x1 = ${x1}`;
+    result2 = `x2 = ${x2}`;
   } else if (D === 0) {
     var x = (-b / (2 * a)).toFixed(2);
-    result = `[-] x = ${x}`;
+    result = `x = ${x}`;
   } else {
     result = "The equation admits no solution";
   }
-  const answer = document.getElementById("answer");
-  const answer2 = document.getElementById("answer2");
-  answer.innerHTML = result;
+  const answer = document.getElementById("answer").innerHTML;
+  const answer2 = document.getElementById("answer2").innerHTML;
+  answer = result;
   if (result2) {
-    answer2.innerHTML = result2;
+    answer2 = result2;
   } else {
-    answer2.innerHTML = null
+    answer2 = null;
   }
 });
 
@@ -33,7 +33,7 @@ const inputFields = document.getElementsByClassName("input-nums");
 
 for (let i = 0; i < inputFields.length; i++) {
   inputFields[i].addEventListener("input", function () {
-    const value = this.value; // Use `this` to refer to the current input element
+    const value = this.value;
     const valueLength = value.length;
     if (valueLength > 25) return;
     const newWidth = Math.max(50, valueLength * 5);
