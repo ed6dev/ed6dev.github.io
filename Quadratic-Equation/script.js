c5a2286b9edabd20e4e345279363c9c7015a9a74
ed6dev <5adnanechetaini5@gmail.com>
2023-11-07 22:36:11 +0100

    added Scientific Notation

--- Quadratic-Equation/script.js
+++ Quadratic-Equation/script.js
@@ -1,31 +1,62 @@
+const toggleSwitch = document.getElementById("toggle");
+function toScientificNotation(number) {
+  // Handle the case where the number is 0 separately
+  if (number === 0) {
+    return "0";
+  }
+
+  const exponent = Math.floor(Math.log10(Math.abs(number)));
+  const coefficient = number / Math.pow(10, exponent);
+  let toreturn;
+  if (exponent !== 0) {
+    toreturn = `${coefficient} * 10^${exponent}`;
+  } else {
+    toreturn = number;
+  }
+  return toreturn;
+}
+
+function getnum(num) {
+  let toreturn;
+  if (toggleSwitch.checked) {
+    toreturn = toScientificNotation(num);
+  } else {
+    toreturn = num;
+  }
+  return toreturn;
+}
+
 document.getElementById("calc").addEventListener("submit", function (event) {
   event.preventDefault();
   const a = document.getElementById("a").value;
   const b = document.getElementById("b").value;
   const c = document.getElementById("c").value;
-
-  var D = b ** 2 - 4 * a * c;
-
-  let result;
-  let result2;
-  if (D > 0) {
-    var x1 = ((-b - Math.sqrt(D)) / (2 * a)).toFixed(2);
-    var x2 = ((-b + Math.sqrt(D)) / (2 * a)).toFixed(2);
-    result = `x1 = ${x1}`;
-    result2 = `x2 = ${x2}`;
-  } else if (D === 0) {
-    var x = (-b / (2 * a)).toFixed(2);
-    result = `x = ${x}`;
-  } else {
-    result = "The equation admits no solution";
-  }
-  const answer = document.getElementById("answer");
-  const answer2 = document.getElementById("answer2");
-  answer.innerHTML = result;
-  if (result2) {
-    answer2.innerHTML = result2;
-  } else {
-    answer2.innerHTML = null;
+  console.log(`a = ${a}\nb = ${b}\nc = ${c}`);
+  try {
+    var D = b ** 2 - 4 * a * c;
+    let result;
+    let result2;
+    if (D > 0) {
+      var x1 = (-b - Math.sqrt(D)) / (2 * a);
+      var x2 = (-b + Math.sqrt(D)) / (2 * a);
+      result = `x1 = ${getnum(x1)}`;
+      result2 = `x2 = ${getnum(x2)}`;
+    } else if (D === 0) {
+      var x = -b / (2 * a);
+      result = `x = ${getnum(x)}`;
+    } else {
+      result = "The equation admits no solution";
+    }
+    const answer = document.getElementById("answer");
+    const answer2 = document.getElementById("answer2");
+    answer.innerHTML = result;
+    if (result2) {
+      answer2.innerHTML = result2;
+    } else {
+      answer2.innerHTML = null;
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
 
